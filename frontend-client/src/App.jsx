3adf78c3f817ee3b4736a50afa2ba02af582ef87
Aakash <editsky597@gmail.com>
2025-06-15 17:37:34 +0530

    versel fix

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import ProfilePage from "./pages/ProfilePage";
-import Navbar from "./components/NavBar";
-import Home from "./pages/HomePage";
-import Home2 from "./pages/Home2"
+
+import Home2 from "./pages/Home2";
 import RegisterPage from "./pages/Register";
 import Login from "./pages/Login";
 import UserDashboard from "./pages/UserDashboard";
@@ -12,31 +10,30 @@ import Link2 from "./pages/Link2";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home2 />} />
 
-return (
-  <div>
-    <Routes>
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<Login />} />
-      
-      <Route path="/" element={<Home2/>} />
-      <Route path="*" element={<NotFoundPage/>}/>
+        {/* Protected route */}
+        <Route
+          path="/link/dashboard"
+          element={
+            <ProtectedRoute>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/link/dashboard"
-        element={
-          <ProtectedRoute>
-            <UserDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/:username" element={<Link2/>} />
+        {/* Dynamic route */}
+        <Route path="/:username" element={<Link2 />} />
 
-      {/* <Route path="/:username" element={<ProfilePage />} /> */}
-    </Routes>
-  </div>
-);
-
-}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
