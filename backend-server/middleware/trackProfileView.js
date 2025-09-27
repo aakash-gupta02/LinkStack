f9c7505f0e3ce98cf00b156557d86fd5d9c7f96d
Aakash <editsky597@gmail.com>
2025-09-27 22:11:03 +0530

    profile tracking number fixed

--- backend-server/middleware/trackProfileView.js
+++ backend-server/middleware/trackProfileView.js
@@ -1,30 +1,13 @@
-import geoip from 'geoip-lite';
 import User from '../models/userModel.js';
 
-export const trackProfileView = async (req, username) => {  // Add req parameter
+export const trackProfileView = async (req, username) => {
   try {
-    const update = { $inc: { profileViews: 1 } };
-    
-    // Get client IP from request
-    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    
-    // Get country if geoip is enabled
-    if (geoip) {
-      const country = geoip.lookup(clientIp)?.country;
-      if (country) {
-        update.$addToSet = { viewerCountries: country };
-      }
-    }
-    
-    // Atomic update with upsert to ensure counters exist
     await User.findOneAndUpdate(
       { username },
-      update,
-      { upsert: true }  // Creates doc if doesn't exist
+      { $inc: { profileViews: 1 } },
+      { upsert: true }
     );
-    
   } catch (err) {
     console.error("Profile view tracking failed:", err);
   }
 };
-
